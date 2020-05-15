1ed4aae97449b3cdadfd2ec3316106551d37045e
duyabo <duyabo@hundun.cn>
2020-05-16 00:57:08 +0800

    U proto: 增加一个proto文件，并把server加进来

--- helloworld_grpc_server.py
+++ helloworld_grpc_server.py
@@ -5,6 +5,8 @@ import time
 import grpc
 from grpc_proto import helloworld_pb2
 from grpc_proto import helloworld_pb2_grpc
+from grpc_proto import myworld_pb2
+from grpc_proto import myworld_pb2_grpc
 
 # 实现 proto 文件中定义的 GreeterServicer
 
@@ -20,10 +22,18 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
         return helloworld_pb2.HelloReply(message='hello again {msg}'.format(msg = request.name))
 
 
+class MyGreeter(myworld_pb2_grpc.MyGreeterServicer):
+    # 实现 proto 文件中定义的 rpc 调用
+    def DoSomething(self, request, context):
+        print 'SayHello receive %s' % request.name
+        return myworld_pb2.SomeReply(message = 'hello {msg}'.format(msg = request.name))
+
+
 def serve():
     # 启动 rpc 服务
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    myworld_pb2_grpc.add_MyGreeterServicer_to_server(MyGreeter(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     try:
